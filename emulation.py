b4a0c31fda6364b97538da11376b94d85889354c
Goodsol Lee <gslee2@netlab.snu.ac.kr>
2023-07-11 13:18:41 +0000

    Add interface

--- emulation.py
+++ emulation.py
@@ -4,19 +4,19 @@ import time
 import threading
 import argparse
 
-def set_up_network():
+def set_up_network(interface):
     # Set up the IFB for the ingress traffic control.
-    cmd1 = 'sudo modprobe ifb numifbs=1'
-    cmd2 = 'sudo ip link set dev ifb0 up'
-    cmd3 = 'sudo tc qdisc add dev eth0 handle ffff: ingress'
-    cmd4 = 'sudo tc filter add dev eth0 parent ffff: u32 match u32 0 0 action mirred egress redirect dev ifb0'
+    cmd1 = f'sudo modprobe ifb numifbs=1'
+    cmd2 = f'sudo ip link set dev ifb0 up'
+    cmd3 = f'sudo tc qdisc add dev {interface} handle ffff: ingress'
+    cmd4 = f'sudo tc filter add dev {interface} parent ffff: u32 match u32 0 0 action mirred egress redirect dev ifb0'
 
     subprocess.run(cmd1, shell=True, check=True)
     subprocess.run(cmd2, shell=True, check=True)
     subprocess.run(cmd3, shell=True, check=True)
     subprocess.run(cmd4, shell=True, check=True)
 
-def change_network_conditions(file, direction):
+def change_network_conditions(file, direction, interface):
     df = pd.read_csv(file)  # Read the CSV file.
 
     for index, row in df.iterrows():
@@ -29,7 +29,7 @@ def change_network_conditions(file, direction):
 
         # Create the command string.
         if direction == "downlink":
-            cmd = f'sudo tc qdisc replace dev eth0 root netem rate {bandwidth}mbit delay {latency}ms'
+            cmd = f'sudo tc qdisc replace dev {interface} root netem rate {bandwidth}mbit delay {latency}ms'
         elif direction == "uplink":
             cmd = f'sudo tc qdisc replace dev ifb0 root netem rate {bandwidth}mbit delay {latency}ms'
 
@@ -43,12 +43,13 @@ def main():
     parser = argparse.ArgumentParser(description='Change network conditions.')
     parser.add_argument('--file', type=str, help='The CSV file to read network conditions from.')
     parser.add_argument('--direction', type=str, choices=['uplink', 'downlink'], help='The direction of the network conditions to change.')
+    parser.add_argument('--interface', type=str, help='The network interface to control.')
     args = parser.parse_args()
 
     if args.direction == "uplink":
-        set_up_network()
+        set_up_network(args.interface)
 
-    thread = threading.Thread(target=change_network_conditions, args=(args.file, args.direction))
+    thread = threading.Thread(target=change_network_conditions, args=(args.file, args.direction, args.interface))
     thread.start()
 
 if __name__ == "__main__":
